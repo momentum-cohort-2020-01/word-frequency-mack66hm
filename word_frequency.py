68dae591f18c060c21d5ce997c297355230c62d6
mack66hm <mack66hm@gmail.com>
2020-02-17 18:17:38 -0500

    get punctuation off and lowercase

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,6 @@
+import string
+
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -6,8 +9,27 @@ STOP_WORDS = [
 
 
 def print_word_freq(file):
+    input_str = open_file(file)
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    punctuation = string.punctuation
+    new_str = input_str
+    print(input_str)
+    for char in input_str:
+        if char in punctuation:
+            new_str = new_str.replace(char, "")
+    print(new_str)
+    print(new_str.lower())
+    new_str = new_str.lower()
+    list_of_words = new_str.split()
+    print(list_of_words)
+
+
+def open_file(file):
+    with open(file) as file:
+        open_file = file.read()
+        print(open_file)
+    return open_file
+    # pass
 
 
 if __name__ == "__main__":
